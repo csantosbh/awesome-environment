bf6d3604a15a1620d81133574f57f7434f641097
Claudio Fernandes <claudiosf1@hotmail.com>
2020-09-28 13:56:06 +0200

    Add .vimrc and zsh_extensions.zsh

---
+++ shell/.vimrc
@@ -0,0 +1,12 @@
+set tabstop=4
+set shiftwidth=4
+set expandtab
+set incsearch
+set hlsearch
+set ignorecase
+set smartcase
+set number
+set autoindent
+set smartindent
+map k gk
+map j gj
---
+++ shell/zsh_extensions.zsh
@@ -0,0 +1,18 @@
+## Put this file into ~/.oh-my-zsh/custom
+
+# Git aliases
+alias gs='git status'
+alias gl='git log'
+alias gd='git diff'
+alias gp='git pull'
+alias gap='git add -p'
+alias gcp='git checkout -p'
+alias g='git'
+
+# Improve ls
+alias ls='ls -a --color=auto'
+
+# Usage: `fnd <name-portion>` will look for files that contain <name-portion> (case insesitive)
+function fnd() {
+    find -iname "*$1*"
+}
