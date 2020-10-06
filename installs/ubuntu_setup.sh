77f11c79251fcf163f6dd13d593040ae8ffc1d1a
Claudio Fernandes <claudiosf1@hotmail.com>
2020-10-06 10:10:06 +0200

    Fix installation script

--- installs/ubuntu_setup.sh
+++ installs/ubuntu_setup.sh
@@ -16,14 +16,20 @@ sudo apt install -y \
     libglm-dev \
     liboctave-dev
 
-# Install anaconda
-cd /tmp/
-ANACONDA_SH=Anaconda3-2020.07-Linux-x86_64.sh
-wget https://repo.anaconda.com/archive/${ANACONDA_SH}
-bash ./${ANACONDA_SH}
-
 # Install and configure oh-my-zsh
+export RUNZSH=no
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install anaconda
+ANACONDA_SH=Anaconda3-2020.07-Linux-x86_64.sh
+if [ ! -f ${ANACONDA_SH} ]; then
+	wget https://repo.anaconda.com/archive/${ANACONDA_SH}
+fi
+bash ./${ANACONDA_SH}
+rm ${ANACONDA_SH}
+if [ -d ~/anaconda3/ ]; then
+	~/anaconda3/bin/conda init zsh
+fi
 
 # Copy configuration files to proper folders
 cp ../shell/.ideavimrc ../shell/.vimrc ~/
@@ -34,7 +40,9 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
           curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Install vim plugins
-git clone https://github.com/easymotion/vim-easymotion ~/.vim/bundle/vim-easymotion
+if [ ! -d ~/.vim/bundle/vim-easymotion ]; then
+    git clone https://github.com/easymotion/vim-easymotion ~/.vim/bundle/vim-easymotion
+fi
 
 # Configure git
 git config --global core.editor "vim"
